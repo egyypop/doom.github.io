b98c0826ab4d24a8973db3fba21ac47c93c1a8df
egyypop <homkitv777@gmail.com>
2024-12-23 19:55:25 +0300

    branch

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-btn = document.querySelector("button")
+let btn = document.querySelector("button")
+function sayHi() {
+    alert('Привет');
+    btn.style.display = "none"
+  }
 btn.addEventListener('click', function(){
-    alert('вы перешли')
+    anime({
+        targets: 'button',
+        keyframes: [
+            {borderRadius: 0},
+            {borderRadius: '50% 50%'},
+            {borderRadius: '50% 20%'},
+            {borderRadius: '20% 20%'},
+            {borderRadius: '50% 50%'},
+            {borderRadius: '0% 0%'}
+        ],
+        duration: 2000,
+        easing: 'linear',
+    })
+    
+      
+      setTimeout(sayHi, 2000);
+      let img = document.querySelector(".img")
+      img.style.display = "none"
+
+      
 })
